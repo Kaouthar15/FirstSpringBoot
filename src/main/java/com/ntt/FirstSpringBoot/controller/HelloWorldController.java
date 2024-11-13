8ea19919d88563b7144023ff30d07fc4d4d3b562
kaoutar <elazamikaoutar@gmail.com>
2024-11-13 15:04:10 +0100

    first try

---
+++ src/main/java/com/ntt/FirstSpringBoot/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.ntt.FirstSpringBoot.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloWorldController {
+	private String message = "Hello World ! ";
+	private String username = "SpringBoot developer";
+	
+	@RequestMapping("/hello")
+	public String welcome(Map<String, Object> model) { 
+		model.put("message",this.message);
+		model.put("name",this.username);
+		return "helloworld";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+}
